de9b83c4fbe3832ba58115402046d2f5a73afcbb
Parantapa Bhattacharya <pb@parantapa.net>
2019-10-19 20:56:17 -0400

    Move from org.json to gson

--- src/main/java/org/bii/nssac/matrix_client/App.java
+++ src/main/java/org/bii/nssac/matrix_client/App.java
@@ -1,6 +1,7 @@
 package org.bii.nssac.matrix_client;
 
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import org.bii.nssac.matrix_client.BagOfPrimitives;
 
 /**
  * Hello world!
@@ -10,29 +11,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        JSONObject tomJsonObj = new JSONObject();
-        tomJsonObj.put("name", "Tom");
-        tomJsonObj.put("birthday", "1940-02-10");
-        tomJsonObj.put("age", 76);
-        tomJsonObj.put("married", false);
 
-        // Cannot set null directly
-        tomJsonObj.put("car", JSONObject.NULL);
+        BagOfPrimitives obj = new BagOfPrimitives();
+        Gson gson = new Gson();
+        String json = gson.toJson(obj);
 
-        tomJsonObj.put("favorite_foods", new String[] { "cookie", "fish", "chips" });
-
-        // {"id": 100001, "nationality", "American"}
-        JSONObject passportJsonObj = new JSONObject();
-        passportJsonObj.put("id", 100001);
-        passportJsonObj.put("nationality", "American");
-        // Value of a key is a JSONObject
-        tomJsonObj.put("passport", passportJsonObj);
-
-        if (true) {
-            // With four indent spaces
-            System.out.println(tomJsonObj.toString(4));
-        } else {
-            System.out.println(tomJsonObj.toString());
-        }
+        System.out.println(json);
     }
 }
