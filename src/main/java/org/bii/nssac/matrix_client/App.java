2ec75d283d1f59e26c7a4ea042a916dc539505e8
Parantapa Bhattacharya <pb@parantapa.net>
2019-10-31 17:45:07 -0400

    Remove unnecesary imports from App.java

--- src/main/java/org/bii/nssac/matrix_client/App.java
+++ src/main/java/org/bii/nssac/matrix_client/App.java
@@ -1,12 +1,7 @@
 package org.bii.nssac.matrix_client;
 
-import java.util.UUID;
 import java.util.ArrayList;
 import java.util.logging.Logger;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import java.util.logging.Level;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -20,7 +15,7 @@ import org.apache.commons.cli.ParseException;
 public class App
 {
     private static final Logger LOG = Logger.getLogger(App.class.getName());
-    
+
     public static void main( String[] args )
     {
         // Define the command line options
@@ -41,41 +36,41 @@ public class App
             formatter.printHelp("bluepill-client", options);
             return;
         }
-        
+
         if (cmd.hasOption("help")) {
             formatter.printHelp("bluepill-client", options);
             return;
         }
-        
-        // Parse the options        
+
+        // Parse the options
         int num_agent_threads = Integer.parseInt(cmd.getOptionValue("n", "1"));
         int num_agents_per_thread = Integer.parseInt(cmd.getOptionValue("m", "1"));
         int start_agent_index = Integer.parseInt(cmd.getOptionValue("s", "0"));
         String address = cmd.getOptionValue("h", "localhost");
         int port = Integer.parseInt(cmd.getOptionValue("p", "16001"));
-        
-        // Chreate the data store        
+
+        // Chreate the data store
         BluePillStore store = new BluePillStore();
-        
+
         // Create a thread list
         ArrayList<Thread> threads = new ArrayList<Thread>();
-        
+
         // Create a the agent threads
         for (int agentproc_id = 0; agentproc_id < num_agent_threads; agentproc_id++) {
             int start_agent_id = start_agent_index + (agentproc_id) * num_agents_per_thread;
             BluePillAgentThread agent_thread = new BluePillAgentThread(agentproc_id, start_agent_id, num_agents_per_thread, store, address, port);
             threads.add(new Thread(agent_thread));
         }
-        
+
         // Create the store thread
         BluePillStoreThread store_thread = new BluePillStoreThread(0, store, address, port);
         threads.add(new Thread(store_thread));
-        
+
         // Start all threads
         for (Thread thread: threads) {
             thread.start();
         }
-        
+
         // Wait for all the threads to finish
         try {
             for (Thread thread: threads) {
